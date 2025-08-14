ae3998a2d80d809e17a7bffd1432bf4a797bad7f
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2025-08-14 17:28:26 +0200

    Add files via upload

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,19 +6,21 @@
 /*   By: rvaz-da- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 18:39:31 by rvaz-da-          #+#    #+#             */
-/*   Updated: 2025/08/11 19:19:00 by rvaz-da-         ###   ########.fr       */
+/*   Updated: 2025/08/13 13:17:44 by rvaz-da-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_BOOLEAN_H
 # define FT_BOOLEAN_H
 
+# include <unistd.h>
+
 typedef int	t_bool;
 
 # define EVEN(nbr) (nbr % 2 == 0)
 # define TRUE 1
 # define FALSE 0
-# define EVEN_MSG "I have an even nnumber of arguments.\n"
+# define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
 # define SUCCESS 0
 
