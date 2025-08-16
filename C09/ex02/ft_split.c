20f8fbdb710e9891848d325966e72806bc90b3d3
rafa-pk <130408957+rafa-pk@users.noreply.github.com>
2025-08-16 11:02:06 +0200

    Add files via upload

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -5,89 +5,112 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: rvaz-da- <rvaz-da-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/13 13:23:31 by rvaz-da-          #+#    #+#             */
-/*   Updated: 2025/08/13 18:03:06 by rvaz-da-         ###   ########.fr       */
+/*   Created: 2025/08/14 13:56:42 by rvaz-da-          #+#    #+#             */
+/*   Updated: 2025/08/14 18:58:13 by rvaz-da-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-/*
- * Plan datak:
- * parser str et compter combien de strings il y a
- * compter combien de chars par string
- * allouer taille totale du tableau + 1
- * parcourir le tableau tant quil est valide
- *a chaque entree du tableau y foutre un string
- *mettre derniere entree a NULL pour marquer la fin du tableau
-*/
-									//TODO chaque char dans charset separe une fois, pas tout les chars
-int	ft_cust_strstr(char *str, char *charset)
+int	char_in_set(char sep, char *charset)
 {
 	int	i;
-	int	j;
-	int	count;
 
 	i = 0;
-	j = 0;
-	count = 0;
-	while (str[i])
+	while (charset[i])
 	{
-		if (str[i] = charset[0])
-		{
-			while (str[i + j] && str[i + j] == charset[j])
-				j++;
-			if (charset[j] == '\0')
-				count++;
-		}
+		if (sep == charset[i])
+			return (1);
+		i++;
 	}
-	return (count);
+	return (0);
 }
 
-char	**alloc_table(char *str, char *charset)
-{
-	int	str_count;
-	char	**table
-	
-	str_count = ft_cust_strstr(str, charset) + 1;
-	table = malloc(sizeof(char	*) * (str_count + 1));
-	if (!table)
-		return (NULL);
-	table[str_count] = NULL;
-	return (table);
-}
-
-char	*ft_cust_strdup(char *str, int *ix, char *charset)
+int	tablen(char *str, char *charset)
 {
 	int	i;
-	int	count;
+	int	strs_to_alloc;
 
-	i = 0
-	while (str[ix])
+	i = 0;
+	strs_to_alloc = 0;
+	while (str[i])
 	{
-		substr[i] = str[ix];
+		if (!char_in_set(str[i], charset))
+		{
+			strs_to_alloc++;
+			while (str[i] && !char_in_set(str[i], charset))
+				i++;
+		}
 		i++;
-		ix++;
 	}
-	substr = '\0';
-	ix += ft_strlen(charset);
+	return (strs_to_alloc + 1);
+}
+
+char	*ft_strdup(char *str, int *ix, char *charset)
+{
+	int		i;
+	int		len;
+	char	*substr;
+
+	i = 0;
+	len = 0;
+	while (char_in_set(str[*ix + len], charset))
+		len++;
+	substr = malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	while (*ix < len)
+	{
+		substr[i] = str[*ix];
+		(*ix)++;
+		i++;
+	}
+	substr[i] = '\0';
+	while (!char_in_set(str[*ix], charset))
+		(*ix)++;
 	return (substr);
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	int	i;
-	int	ix;
+	int		i;
+	int		ix;
+	int		strcount;
 	char	**table;
 
 	i = 0;
 	ix = 0;
-	table = alloc_table(char *str, char *charset);
-	while (table[i] != NULL)	//parcours le tableau
+	strcount = tablen(str, charset);
+	table = malloc(sizeof(char	*) * (strcount + 1));
+	if (!table)
+		return (NULL);
+	while (i < strcount)
 	{
-		table[i] = ft_cust_strdup(str, &ix, charset); //copie str apd de ix jusqua charset, saute jusquau str dapres
+		table[i] = ft_strdup(str, &ix, charset);
 		i++;
 	}
+	table[i] = NULL;
 	return (table);
+}
+
+int	main(void)
+{
+	char	str[] = "wesh, wesh, canapeche";
+	char	charset[] = ", ";
+	char	**table = ft_split(str, charset);
+	int		i = 0;
+	
+	while (i < 4)
+	{
+		printf("table[%d]: %s\n", i, table[i]);
+		i++;
+	}
+	while (i >= 0)
+	{
+		free(table[i]);
+		i--;
+	}
+	free(table);
+	return (0);
 }
